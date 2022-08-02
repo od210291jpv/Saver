5fe6b07f57b8785c5adf12bfffd21ebdd9713afb
Me me <od210291jpv@gmail.com>
2022-08-02 15:32:02 +0300

    Updated creds for db

--- SaverBackend/Models/ApplicationContext.cs
+++ SaverBackend/Models/ApplicationContext.cs
@@ -4,7 +4,7 @@ namespace SaverBackend.Models
 {
     public class ApplicationContext : DbContext
     {
-        public static string ConnectionString = "Database=mobilesdb;Uid=root;Pwd=password;";
+        public static string ConnectionString = "Database=mobilesdb;Uid=user;Pwd=password;";
 
         public DbSet<Category> Categories { get; set; }
         public DbSet<Content> Contents { get; set; }
@@ -14,10 +14,5 @@ namespace SaverBackend.Models
         {
             Database.EnsureCreated();
         }
-
-        //protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
-        //{
-        //    optionsBuilder.UseSqlServer(ConnectionString);
-        //}
     }
 }
